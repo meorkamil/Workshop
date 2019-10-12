2aea92a0458cc5ccdeb81af1ebc6f1b090152316
meorkamil <36105680+meorkamil@users.noreply.github.com>
2019-10-12 23:15:19 +0800

    first commit

---
+++ MagiClean/src/app/home/home.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Policy } from  './../policy';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  username: string = " ";
+  password: string = " ";
+
+  constructor() {}
+
+ loginProcess(){
+   
+
+ 
+
+
+ }
+}
---
+++ MagiClean/src/app/policy.ts
@@ -0,0 +1,10 @@
+export  class  Policy {
+    id: number;
+    number:  number;
+    amount:  number;
+}
+export  class  admin {
+    username: string;
+    password:  string;
+   
+}
